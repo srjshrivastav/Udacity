7bcc064920a0d4aa97f75d62ee5d00802608dc70
Suraj Shrivastava <srjshrivastv1@gmail.com>
2020-05-06 00:46:58 +0530

    Use Redux library instead of own code

--- Redux/index.js
+++ Redux/index.js
@@ -1,35 +1,35 @@
 //Library code
 
-function createStore(reducer){
-    // the Store should have four parts
-    //1. the state
-    //2. get the state
-    //3. Listen to changes on the state.
-    //4. update the state
+// function createStore(reducer){
+//     // the Store should have four parts
+//     //1. the state
+//     //2. get the state
+//     //3. Listen to changes on the state.
+//     //4. update the state
 
-    let state
-    let listneres=[]
+//     let state
+//     let listneres=[]
 
-    const subscribe=(listnere)=>{
-        listneres.push(listnere)
-        return ()=>{
-            listneres=listneres.filter((l)=>l !== listnere)
-        }
-    }
-    const dispatch=(action)=>{
-        //call the function to change the state
-        state=reducer(state,action)
-        listneres.forEach((listnere)=>listnere())
-    }
+//     const subscribe=(listnere)=>{
+//         listneres.push(listnere)
+//         return ()=>{
+//             listneres=listneres.filter((l)=>l !== listnere)
+//         }
+//     }
+//     const dispatch=(action)=>{
+//         //call the function to change the state
+//         state=reducer(state,action)
+//         listneres.forEach((listnere)=>listnere())
+//     }
 
-    const getState=()=> state
+//     const getState=()=> state
 
-    return{
-        getState,
-        subscribe,
-        dispatch
-    }
-}
+//     return{
+//         getState,
+//         subscribe,
+//         dispatch
+//     }
+// }
 
 
 //Action Types
@@ -75,7 +75,6 @@ function removeGoalAction(id){
 }
 //***************************end******************************
 function todos(state=[],action){
-    console.log('my',state)
     switch(action.type){
         case ADD_TODO:
             return state.concat([action.todo])
@@ -104,13 +103,16 @@ function goals(state=[],action){
 function generateId(){
     return Math.random().toString(36).substring(2)+(new Date()).getTime().toString(36)
 }
-function app(state={},action){
-    return {
-        todos : todos(state.todos,action),
-        goals : goals(state.goals,action)
-    }
-}
-const store = createStore(app)
+// function app(state={},action){
+//     return {
+//         todos : todos(state.todos,action),
+//         goals : goals(state.goals,action)
+//     }
+// }
+const store = Redux.createStore(Redux.combineReducers({
+    todos,
+    goals
+}))
 
 store.subscribe(()=>{
     const {todos, goals} = store.getState()
