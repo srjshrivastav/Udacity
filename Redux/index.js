e924bf88b39c3336f8bc659ad2415bc8541c9a74
Suraj Shrivastava <srjshrivastv1@gmail.com>
2020-05-05 20:16:48 +0530

    ADD MORE FEATURE TO TODO

--- Redux/index.js
+++ Redux/index.js
@@ -35,10 +35,18 @@ function createStore(reducer){
 
 //App Code
 function todos(state=[],action){
-    if(action.type==='ADD_TODO')
-        return state.concat([action.todo])
-
-    return state
+    switch(action.type){
+        case 'ADD_TODO':
+            return state.concat([action.todo])
+        case 'REMOVE_TODO':
+            return state.filter((todo)=>todo.id!==action.id)
+        case 'TOGGLE_TODO':
+            return state.map((todo)=>{
+                todo.id !== action.id?todo:Object.assign({},todo,{complete:!todo.complete})
+            })
+        default:
+            return state
+    }
 }
 
 const store = createStore(todos)
