f4c47f899124a28437e77ce946a16777607b7c6a
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-05-08 19:32:03 +0530

    Create Store

--- Twitter-kindOf/src/index.js
+++ Twitter-kindOf/src/index.js
@@ -2,5 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducers from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducers)
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, document.getElementById('root'))
