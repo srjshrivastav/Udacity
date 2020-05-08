f0c01a525273a6f952e6772ff2ef36a94f9beb03
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-05-08 20:21:05 +0530

    Created dashboard component

--- Twitter-kindOf/src/components/App.js
+++ Twitter-kindOf/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {handleInitialData} from '../actions/shared'
-import thunk from 'redux-thunk'
+import Dashboard from './Dashboard'
 
 class App extends Component {
   componentDidMount(){
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+       <Dashboard />
       </div>
     )
   }
